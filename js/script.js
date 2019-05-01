5d2feaf43cf60abd36ff62f14bae9891ddd596d2
samantas <samantas@nyu.edu>
2019-05-01 09:45:37 +0800

    toggle display after load

--- js/script.js
+++ js/script.js
@@ -3,42 +3,43 @@ function init() {
         $('#hero').removeClass("flex-row");
         $('#hero').addClass("flex-column");
         $('#taglineSection').addClass("center");
-        $('.hero-content').css('margin-bottom','25px');
+        $('.hero-content').css('margin-bottom', '25px');
         $('#childDiv').removeClass("flex-row");
         $('#childDiv').addClass("flex-column");
-        $('.section').css('max-height','unset');
+        $('.section').css('max-height', 'unset');
         $('#childDiv > div:nth-child(1)').removeClass("container");
         $('#childDiv > div.container.flex-column.justify-center.padding-left').addClass("center");
-        $('body > div.footer > div.footer-bottom > div').css('flex-direction','column');
+        $('body > div.footer > div.footer-bottom > div').css('flex-direction', 'column');
         $('#featureOverviewSubheader').removeClass("cushion");
-        $('#featureOverviewSubheader').css('margin','20px 0 0');
+        $('#featureOverviewSubheader').css('margin', '20px 0 0');
 
         $('#hi-p').removeClass("width-half");
 
         $('#menuNavItems > div.nav-item.btn-primary').addClass('hidden');
         $('#menuNavItems > .nav-item').addClass('nav-item-mobile');
-        
-        $('#navMobileIcon').click(function(){
-        	$('#menuNavItems').addClass('nav-mobile-overlay');
-        	$('#menuNavItems').removeClass('nav-desktop');
+
+        $('#navMobileIcon').click(function() {
+            $('#menuNavItems').addClass('nav-mobile-overlay');
+            $('#menuNavItems').removeClass('nav-desktop');
         });
 
         $('.close-icon').removeClass("hidden");
 
-        $('#closeMobileMenu').click(function(){
-        	$('#menuNavItems').removeClass('nav-mobile-overlay');
-        	$('#menuNavItems').addClass('nav-desktop');
+        $('#closeMobileMenu').click(function() {
+            $('#menuNavItems').removeClass('nav-mobile-overlay');
+            $('#menuNavItems').addClass('nav-desktop');
         });
 
         $('#hiwStepsContainer').addClass("flex-column");
 
 
     } else {
-    	$('.close-icon').addClass("hidden");
-    	$('#hi-p').addClass("width-half");
-    	$('#hiwStepsContainer').removeClass("flex-column");
+        $('.close-icon').addClass("hidden");
+        $('#hi-p').addClass("width-half");
+        $('#hiwStepsContainer').removeClass("flex-column");
     }
 
 }
 
-$(document).ready(init);
+$(function() { document.body.style.display = "block";
+    init() });
